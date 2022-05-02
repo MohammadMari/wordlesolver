0e76c0c12b3110db8f3a5d9ebaf4c2f30da93afa
SergeantMeatWad <104181364+SergeantMeatWad@users.noreply.github.com>
2022-05-01 20:07:23 -0500

    Update wordStrength.h

--- wordlesolver/wordStrength.h
+++ wordlesolver/wordStrength.h
@@ -1,4 +1,22 @@
 #pragma once
+#include <string>
+
+using namespace std;
+
+class wordStrength
+{
+public:
+	wordStrength() {};
+	wordStrength(string word)
+	{
+		this->word = word;
+		calcWordStrength();
+	}
+	void calcWordStrength();
+	string word;
+	float strength = 0;
+
+};
 
 
 // sort words by strength (best to guess first)
