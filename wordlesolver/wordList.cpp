7a732de9a13b02c61f2405f1a9e2481d54647a60
mtm0227 <64770850+MohammadMari@users.noreply.github.com>
2022-05-07 23:20:43 -0500

    forgot to clean header file

--- wordlesolver/wordList.cpp
+++ wordlesolver/wordList.cpp
@@ -45,3 +45,81 @@ void wordList::readFile()
 		push_back(word);
 	}
 }
+
+
+string wordList::best()
+{
+	wordNode* node = words;
+
+	if (!node)
+		return "";
+
+	wordNode* bestPtr = nullptr;
+	float bestAmt = INT_MIN;
+
+	while (node->next)
+	{
+		if (node->wordStr.strength > bestAmt)
+		{
+			bestAmt = node->wordStr.strength;
+			bestPtr = node;
+		}
+
+		node = node->next;
+	}
+
+	return bestPtr->word;
+}
+
+
+
+int wordList::size()
+{
+	if (!words)
+		return 0;
+
+	int cnt = 1;
+
+	wordNode* child = words;
+
+	while (child->next)
+	{
+		child = child->next;
+		cnt++;
+	}
+
+	return cnt;
+}
+
+string wordList::front()
+{
+	if (words)
+		return words->word; // return first word
+	return "";
+}
+
+void wordList::push_back(string str)
+{
+	if (!words)
+		words = new wordNode(str);
+	wordNode* child = words;
+
+	while (child->next)
+		child = child->next;
+
+	child->next = new wordNode(str);
+}
+
+string wordList::at(int i)
+{
+	int cnt = 0;
+	wordNode* ret = words;
+
+	while ((cnt < i) && ret->next)
+	{
+		ret = ret->next;
+		cnt++;
+	}
+
+	return ret->word;
+}
--- wordlesolver/wordList.h
+++ wordlesolver/wordList.h
@@ -29,80 +29,9 @@ public:
 	void remove(int i); // or maybe an index
 	void readFile();
 
-	string best()
-	{
-		wordNode* node = words;
-
-		if (!node)
-			return "";
-
-		wordNode* bestPtr = nullptr;
-		float bestAmt = INT_MIN;
-
-		while (node->next)
-		{
-			if (node->wordStr.strength > bestAmt)
-			{
-				bestAmt = node->wordStr.strength;
-				bestPtr = node;
-			}
-
-			node = node->next;
-		}
-
-		return bestPtr->word;
-	}
-
-
-	
-	int size()
-	{
-		if (!words)
-			return 0;
-
-		int cnt = 1;
-
-		wordNode* child = words;
-
-		while (child->next)
-		{
-			child = child->next;
-			cnt++;
-		}
-
-		return cnt;
-	}
-
-	string front()
-	{
-		if (words)
-			return words->word; // return first word
-		return "";
-	}
-
-	void push_back(string str)
-	{
-		if (!words)
-			words = new wordNode(str);
-		wordNode* child = words;
-
-		while (child->next)
-			child = child->next;
-
-		child->next = new wordNode(str);
-	}
-
-	string at(int i)
-	{
-		int cnt = 0;
-		wordNode* ret = words;
-
-		while ((cnt < i) && ret->next)
-		{
-			ret = ret->next;
-			cnt++;
-		}
-
-		return ret->word;
-	}
+	string best();
+	int size();
+	string front();
+	void push_back(string str);
+	string at(int i);
 };
